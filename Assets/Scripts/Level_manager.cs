b1f8c97de0800163c92aa4eee3bb8ce0de644d02
Blaire Jung <seoeun.jungg@gmail.com>
2025-02-17 20:50:21 -0600

    Added Pause Menu

--- Assets/Scripts/Level_manager.cs
+++ Assets/Scripts/Level_manager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Level_manager : MonoBehaviour
 {
     public static Level_manager instance;
+    private bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+    public Button resumeButton;
+    public Button quitButton;
 
     private void Awake() //Makes levelmanager callable in any script: Level_manager.instance.[]
     {
@@ -14,18 +20,33 @@ public class Level_manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        resumeButton.onClick.AddListener(TogglePause);
+        quitButton.onClick.AddListener(QuitGame);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Dummy")){
-            Dummy();
+        if(Input.GetKeyDown(KeyCode.P)){ // Detects when 'P' is pressed
+            TogglePause();
         }
     }
 
-    void Dummy(){
-        Debug.Log("Dummy key pressed");
+    void TogglePause(){
+        isPaused = !isPaused;
+
+        if (isPaused) {
+            pauseMenuUI.SetActive(true); // show menu
+            Time.timeScale = 0f; // pause game
+        }
+        else {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f; // resume game
+        }
+    }
+
+    void QuitGame()
+    {
+        Application.Quit(); // quitting game
     }
 }
