be7072b56b26816e3c126cb2ee0b933db2920b47
ambrosia13 <aavyakumbhare@gmail.com>
2025-02-17 20:36:33 -0600

    game state singleton

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Singleton holding all the game constants
+public class GameState : MonoBehaviour
+{
+    private static GameState instance;
+
+    public static GameState Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Initialize();
+                Debug.Log("GameState singleton initialized");
+            }
+
+            return instance;
+        }
+    }
+
+    public int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 7;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Use this for intiialization logic instead of Start(), because Start() may be called
+    // too late for other scripts to see data in this object.
+    private static void Initialize()
+    {
+        instance = new GameObject().AddComponent<GameState>();
+        instance.name = "Game State";
+
+
+
+        DontDestroyOnLoad(instance.gameObject);
+    }
+}
