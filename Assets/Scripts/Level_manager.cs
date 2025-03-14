ccb5985096e624b1ef9b5032ad0ef5185c551051
Rockyd3 <legodaehler03@gmail.com>
2025-03-13 19:36:30 -0500

    Updated Visuals for all but Shop room, added UI

--- Assets/Scripts/Level_manager.cs
+++ Assets/Scripts/Level_manager.cs
@@ -30,6 +30,9 @@ public class Level_manager : MonoBehaviour
 
     [SerializeField] private ProgressBar healthBar;
     [SerializeField] private TMP_Text T_Currency;
+    [SerializeField] public float PlayerHealth = -1;
+    [SerializeField] public float PlayerMaxHealth = 20;
+
 
     public float UpgradeIconUnplugOffset; // how far the upgrade UI icons "unplug" when you select them
 
@@ -84,12 +87,18 @@ public class Level_manager : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
 
+            //Instantiate the main UI
             GameObject mainUI = Instantiate(mainUIPrefab);
             mainUI.name = "UI";
             DontDestroyOnLoad(mainUI);
 
-            pauseMenuUI = Instantiate(pauseMenuUIPrefab, transform);
+            //Get HUD elements to keep them up to date
+            healthBar = mainUI.GetComponent<ProgressBar>();
+            T_Currency = mainUI.transform.Find("MainCanvas/Healthbar/Currency").GetComponent<TMP_Text>();
 
+            //Instantiate the Pause Menu
+            pauseMenuUI = Instantiate(pauseMenuUIPrefab, transform);
+            //Get Buttons for PauseMenu
             resumeButton = pauseMenuUI.transform.Find("PauseMenu/ResumeButton").GetComponent<Button>();
             pauseButton = pauseMenuUI.transform.Find("PauseMenu/PauseButton").GetComponent<Button>();
             quitButton = pauseMenuUI.transform.Find("PauseMenu/QuitButton").GetComponent<Button>();
@@ -104,6 +113,9 @@ public class Level_manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     { 
+        // Give self 0 coins to set HUD currency
+        GainCoin(0);
+
         // FIXME: Setting up the Patterns List should be move to gameconstants when one exists.
         //Create lists for all of the Patterns
         List<(int, string)> Temp_1 = new List<(int, string)>();
@@ -193,6 +205,16 @@ public class Level_manager : MonoBehaviour
 
     public void GainCoin(int val) {
         Currency += val;
+        T_Currency.text = Currency.ToString() + " CHIPS";
+    }
+
+    public void SetMaxHealth(float val){
+        PlayerMaxHealth = val;
+    }
+
+    public void SetHealth(float val){
+        PlayerHealth = val;
+        healthBar.SetProgress(PlayerHealth/PlayerMaxHealth);
     }
 
     //---------------------------------Functions for Patterns----------------------------
